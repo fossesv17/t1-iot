ef4d09a92187e03cb6c631c78f35a4ab66d2548d
David Parada <david.parada.munoz@gmail.com>
2023-11-10 19:18:38 -0300

    Added: Inserting functions

--- codigo_rasp/modelos.py
+++ codigo_rasp/modelos.py
@@ -1,7 +1,10 @@
+import datetime
+
 from peewee import Model, PostgresqlDatabase, ForeignKeyField, CharField, IntegerField, FloatField, DateTimeField, \
     BlobField
 
 import psycopg2
+import psycopg2.extras
 
 # Configuración de la base de datos
 hostname = 'localhost'
@@ -16,139 +19,202 @@ db = PostgresqlDatabase('db',
                         user=user,
                         password=password)
 
-conn = None
+
+def insert_to_Datos(headers, dataInDict):
+    """
+    :param headers: Un diccionario con los headers
+    :param dataInDict: Un diccionario con todos los datos que se desean agregar
+    """
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+        id_protocol = headers['ID_protocol']
+
+        if id_protocol == 0:
+            insert_to_datos_script = 'INSERT INTO Datos (IDDevice, Device_MAC, Battlevel) VALUES ' \
+                                     '(%s, %s, %s)'
+
+            insert_to_datos_values = (headers['ID_device'], headers['MAC'], dataInDict['Batt_level'])
+
+            cur.execute(insert_to_datos_script, insert_to_datos_values)
+
+        elif id_protocol == 1:
+            insert_to_datos_script = 'INSERT INTO Datos (IDDevice, Device_MAC, Battlevel, Timestamp) VALUES ' \
+                                     '(%s, %s, %s, %s)'
+
+            insert_to_datos_values = (headers['ID_device'], headers['MAC'], dataInDict['Batt_level'],
+                                      dataInDict['Timestamp'])
+
+            cur.execute(insert_to_datos_script, insert_to_datos_values)
+
+        elif id_protocol == 2:
+            insert_to_datos_script = 'INSERT INTO Datos (IDDevice, Device_MAC, Battlevel, Timestamp, ' \
+                                     'Temp, Press, Hum, Co) VALUES ' \
+                                     '(%s, %s, %s, %s, %s, %s, %s, %s)'
+
+            insert_to_datos_values = (headers['ID_device'], headers['MAC'], dataInDict['Batt_level'],
+                                      dataInDict['Timestamp'], dataInDict['Temp'], dataInDict['Press'],
+                                      dataInDict['Hum'], dataInDict['Co'])
+
+            cur.execute(insert_to_datos_script, insert_to_datos_values)
+        else:
+            insert_to_datos_script = 'INSERT INTO Datos (IDDevice, Device_MAC, Battlevel, Timestamp, ' \
+                                     'Temp, Press, Hum, Co, RMS, Ampx, Frecx, Ampy, Frecy, Ampz, Frecz) VALUES ' \
+                                     '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+
+            insert_to_datos_values = (headers['ID_device'], headers['MAC'], dataInDict['Batt_level'],
+                                      dataInDict['Timestamp'], dataInDict['Temp'], dataInDict['Press'],
+                                      dataInDict['Hum'], dataInDict['Co'], dataInDict['RMS'], dataInDict['Amp_X'],
+                                      dataInDict['Frec_X'], dataInDict['Amp_Y'], dataInDict['Frec_Y'],
+                                      dataInDict['Amp_Z'], dataInDict['Frec_Z'])
+
+            cur.execute(insert_to_datos_script, insert_to_datos_values)
+
+
+def insert_to_Configuracion(headers):
+    """
+    :param headers: Un diccionario con los headers
+    """
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        protocol = headers['ID_protocol']
+        trans_layer = headers['Transport_layer']
+
+        insert_script = 'INSERT INTO Configuracion (IDProtocol, TransportLayer) VALUES (%s, %s)'
+        insert_values = (protocol, trans_layer)
+        cur.execute(insert_script, insert_values)
+
+
+def insert_to_Logs(headers, dataInDict):
+    """
+    :param headers: Un diccionario con los headers
+    :param dataInDict: Un diccionario con todos los datos que se desean agregar
+    """
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        insert_to_logs_script = 'INSERT INTO Logs (Timestamp, IDDevice, IDProtocol, TransportLayer) VALUES ' \
+                                '(%s, %s, %s, %s)'
+
+        insert_to_logs_values = (dataInDict['Timestamp'], headers['ID_device'], headers['ID_protocol'],
+                                 headers['Transport_layer'])
+
+        cur.execute(insert_to_logs_script, insert_to_logs_values)
+
+
+def insert_to_Loss(headers, dataInDict):
+    """
+    :param headers: Un diccionario con los headers
+    :param dataInDict: Un diccionario con todos los datos que se desean agregar
+    """
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        expected_len = headers['length']
+
+        real_len = len(dataInDict.values())
+
+        diff = expected_len - real_len
+
+        insert_to_loss_script = 'INSERT INTO Loss (TiempoDemora, PacketLoss) VALUES (%s, %s)'
+
+        fecha1 = dataInDict['Timestamp']
+        fecha2 = datetime.datetime.now()
+
+        fecha1_hours = int(datetime.datetime.strptime(fecha1, '%Y-%m-%d %H:%M:%S').strftime('%H'))
+        fecha1_minutes = int(datetime.datetime.strptime(fecha1, '%Y-%m-%d %H:%M:%S').strftime('%M'))
+        fecha1_seconds = int(datetime.datetime.strptime(fecha1, '%Y-%m-%d %H:%M:%S').strftime('%S'))
+
+        fecha2_hours = int(fecha2.strftime('%H'))
+        fecha2_minutes = int(fecha2.strftime('%M'))
+        fecha2_seconds = int(fecha2.strftime('%S'))
+
+        seconds = 3600 * (fecha2_hours - fecha1_hours) + 60 * (fecha2_minutes - fecha1_minutes) + \
+                  (fecha2_seconds - fecha1_seconds)
+
+        insert_to_loss_values = (seconds, diff)
+
+        cur.execute(insert_to_loss_script, insert_to_loss_values)
+
+
 cur = None
 
 try:
 
-    conn = psycopg2.connect(host=hostname,
-                            user=user,
-                            password=password)
+    with psycopg2.connect(host=hostname,
+                          user=user,
+                          password=password) as conn:
 
-    cur = conn.cursor()
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            # Baja las tablas en caso de existir previamente
+            cur.execute('DROP TABLE IF EXISTS Datos')
+            cur.execute('DROP TABLE IF EXISTS Configuracion')
+            cur.execute('DROP TABLE IF EXISTS Logs')
+            cur.execute('DROP TABLE IF EXISTS Loss')
 
-    create_datos_script = '''CREATE TABLE IF NOT EXISTS Datos(
-                            IDdevice character(2) NOT NULL,
-                            Device_MAC character(6) NOT NULL PRIMARY KEY,
-                            Battlevel integer,
-                            Timestamp timestamp(4),
-                            Temp integer,
-                            Press integer,
-                            Hum integer,
-                            Co real,
-                            RMS real,
-                            Ampx real,
-                            Frecx real,
-                            Ampy real,
-                            Frecy real,
-                            Ampz real,
-                            Frecz real) '''
+            # Scripts para crear las tablas
+            create_datos_script = '''CREATE TABLE IF NOT EXISTS Datos(
+                                    IDdevice character(8) NOT NULL,
+                                    Device_MAC character(18) NOT NULL,
+                                    Battlevel integer,
+                                    Timestamp timestamp(4),
+                                    Temp integer,
+                                    Press integer,
+                                    Hum integer,
+                                    Co real,
+                                    RMS real,
+                                    Ampx real,
+                                    Frecx real,
+                                    Ampy real,
+                                    Frecy real,
+                                    Ampz real,
+                                    Frecz real) '''
 
-    create_config_script = '''CREATE TABLE IF NOT EXISTS Configuracion(
-                            IDProtocol integer NOT NULL,
-                            Transport_Layer character(1) NOT NULL)'''
+            create_config_script = '''CREATE TABLE IF NOT EXISTS Configuracion(
+                                    IDProtocol integer NOT NULL,
+                                    TransportLayer character(3) NOT NULL)'''
 
-    create_logs_script = '''CREATE TABLE IF NOT EXISTS Logs(
-                            Timestmp timestamp(4) NOT NULL,
-                            IDDevice character(2) NOT NULL,
-                            IDProtocol integer NOT NULL,
-                            Transport_Layer character(1) NOT NULL)'''
+            create_logs_script = '''CREATE TABLE IF NOT EXISTS Logs(
+                                    Timestamp timestamp(4) NOT NULL,
+                                    IDDevice character(8) NOT NULL,
+                                    IDProtocol integer NOT NULL,
+                                    TransportLayer character(3) NOT NULL)'''
 
-    create_loss_script = '''CREATE TABLE IF NOT EXISTS Loss(
-                            TiempoDemora real NOT NULL,
-                            PacketLoss int NOT NULL)'''
+            create_loss_script = '''CREATE TABLE IF NOT EXISTS Loss(
+                                    TiempoDemora real NOT NULL,
+                                    PacketLoss int NOT NULL)'''
 
-    cur.execute(create_datos_script)
-    cur.execute(create_config_script)
-    cur.execute(create_logs_script)
-    cur.execute(create_loss_script)
+            cur.execute(create_datos_script)
+            cur.execute(create_config_script)
+            cur.execute(create_logs_script)
+            cur.execute(create_loss_script)
 
-    insert_to_datos_script = 'INSERT INTO Datos (IDDevice, Device_MAC, Battlevel, Timestamp, ' \
-                             'Temp, Press, Hum, Co, RMS, Ampx, Frecx, Ampy, Frecy, Ampz, Frecz) VALUES ' \
-                             '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            # Ejemplos donde se agregan datos manualmente
 
-    #insert_to_datos_values = ('Harry', '2C:41:A1:27:09:57', 80, '1999-01-08 04:05:06')
+            headers_datos_0 = {"ID_device": 'Harry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 0,
+                               "Transport_layer": 'TCP', "length": 27}
 
-    conn.commit()
+            headers_datos_1 = {"ID_device": 'Henry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 1,
+                               "Transport_layer": 'TCP', "length": 31}
+
+            headers_datos_2 = {"ID_device": 'Larry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 2,
+                               "Transport_layer": 'TCP', "length": 41}
+
+            headers_datos_3 = {"ID_device": 'Jerry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 3,
+                               "Transport_layer": 'TCP', "length": 69}
+
+            headers = (headers_datos_0, headers_datos_1, headers_datos_2, headers_datos_3)
+
+            insert_to_datos_values = {"Batt_level": 80, "Timestamp": '2023-10-08 04:05:06', "Temp": 15, "Press": 1100,
+                                      "Hum": 55, "Co": 176, "RMS": 0.009, "Amp_X": 0.1, "Frec_X": 30.3, "Amp_Y": 0.05,
+                                      "Frec_Y": 59.1, "Amp_Z": 0.009, "Frec_Z": 90.2}
+
+            for header in headers:
+                insert_to_Datos(header, insert_to_datos_values)
+                insert_to_Logs(header, insert_to_datos_values)
+                insert_to_Loss(header, insert_to_datos_values)
+                insert_to_Configuracion(header)
+
 
 except Exception as error:
     print(error)
 finally:
-    if cur is not None:
-        cur.close()
     if conn is not None:
         conn.close()
-
-
-# Definición de un modelo
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class Datos(BaseModel):
-    # Headers necesarios
-    ID_device = CharField(max_length=2)
-    Device_MAC = CharField(max_length=6)
-
-    # Datos Protocolo 0
-    Batt_level = IntegerField()
-
-    # Datos Protocolo 1
-    Timestamp = DateTimeField()
-
-    # Datos Protocolo 2
-    Temp = IntegerField()
-    Press = IntegerField()
-    Hum = IntegerField()
-    Co = FloatField()
-
-    # Datos Protocolo 3
-    RMS = FloatField()
-    Amp_x = FloatField()
-    Frec_x = FloatField()
-    Amp_y = FloatField()
-    Frec_y = FloatField()
-    Amp_z = FloatField()
-    Frec_z = FloatField()
-
-    class Meta:
-        database = db
-        db_table = 'Datos'
-
-
-class Configuracion(BaseModel):
-    # Headers necesarios
-    ID_protocol = CharField(max_length=1)
-    Transport_Layer = CharField(max_length=1)
-
-
-class Logs(BaseModel):
-    timestamp = ForeignKeyField(Datos)
-    ID_device = ForeignKeyField(Datos)
-
-    # Headers necesarios
-    ID_protocol = ForeignKeyField(Configuracion)
-    Transport_Layer = ForeignKeyField(Configuracion)
-
-
-class Loss(BaseModel):
-    Tiempo_Demora = FloatField()
-    Packet_Loss = IntegerField()
-
-
-def update_config(id_protocol, trans_layer):
-    # Tabla Configuracion
-    config = Configuracion.select()
-    if config is not None:
-        for conf in config:
-            if conf.ID_protocol != id_protocol or conf.Transport_Layer != trans_layer:
-                conf.ID_protocol = id_protocol
-                conf.Transport_Layer = trans_layer
-                conf.save()
-    else:
-        new_config = Configuracion.create(ID_protocol=id_protocol, Transport_Layer=trans_layer)
-        new_config.save()
 
 
 def saveData(headers, dataInDict):
@@ -180,6 +246,3 @@ def saveData(headers, dataInDict):
 
     else:
         pass
-
-# db.connect()
-# db.create_tables([Datos, Configuracion, Logs, Loss])
--- codigo_rasp/packet_parser.py
+++ codigo_rasp/packet_parser.py
@@ -2,7 +2,7 @@ from struct import unpack, pack
 from pickle import dumps, loads
 import datetime
 import traceback
-from modelos import * 
+from modelos import *
 
 # Documentación struct unpack,pack :https://docs.python.org/3/library/struct.html#
 
@@ -13,8 +13,10 @@ from modelos import *
 -parseData: Utiliza dataToDict para obtener los datos y luego los almacena en la base de datos
 '''
 
-def response(ID_protocol:int, transport_layer:int):
-    return pack("<BB", ID_protocol,transport_layer)
+
+def response(ID_protocol: int, transport_layer: int):
+    return pack("<BB", ID_protocol, transport_layer)
+
 
 def getHeader(packet):
     header = packet[:12]
@@ -22,61 +24,63 @@ def getHeader(packet):
     print(header)
     return header
 
+
 def headerToDict(data):
     ID_Device, M1, M2, M3, M4, M5, M6, transport_layer, protocol, len_msg = unpack("<2B6BBB2B", data)
     MAC = ".".join([hex(x)[2:] for x in [M1, M2, M3, M4, M5, M6]])
-    return {"ID_device":ID_Device, "MAC":MAC, "ID_protocol":protocol, "Transport_layer":transport_layer, "length":len_msg}
+    return {"ID_device": ID_Device, "MAC": MAC, "ID_protocol": protocol, "Transport_layer": transport_layer,
+            "length": len_msg}
+
 
 def getData(packet):
     d1 = packet[0]
-    dictData = d1 #unpack("<B",d1)
+    dictData = d1
     return dictData
+
 
 def parseData(header, packet):
     dictData = dataToDict(header["ID_protocol"], packet)
     if dictData is not None:
         # Falta definir la funcion saveData en los modelos
-        saveData(header, dictData)
+        insert_to_Datos(header, dictData)
         print(dictData)
-        
+
     return None if dictData is None else {**header, **dictData}
 
-def protocolUnpack(protocol:int, data):
-    protocol_unpack = ["<B", "<B4B", "<B4BB4BBf", "<B4BB4BBffffffff", "<B4BB4BBf2000f2000f2000f2000f2000f2000f"]
+
+def protocolUnpack(protocol: int, data):
+    protocol_unpack = ["<B", "<B4B", "<B4BB4BBf", "<B4BB4BBffffffff"]
     return unpack(protocol_unpack[protocol], data)
 
 
-def dataToDict(protocol:int, data):
-    if protocol not in [0, 1, 2, 3, 4]:
+def dataToDict(protocol: int, data):
+    if protocol not in [0, 1, 2, 3]:
         print("Error: protocol doesnt exist")
         return None
-    
+
     def protocolFunc(protocol, keys):
         def p(data):
             unp = protocolUnpack(protocol, data)
             data_dict = {}
-            for (key,val) in zip(keys,unp):
+            for (key, val) in zip(keys, unp):
                 if key == "Timestamp":
                     data_dict[key] = datetime.datetime.now()
-                elif key in ["Acc_X", "Acc_Y", "Acc_Z", "Rgyr_X", "Rgyr_Y", "Rgyr_Z"]:
-                    data_dict[key] = dumps(val)
                 else:
                     data_dict[key] = val
             return data_dict
+
         return p
-    
+
     p0 = ["Batt_level"]
     p1 = ["Batt_level", "Timestamp"]
     p2 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co"]
-    p3 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co", "RMS", "Amp_X", "Frec_X", "Amp_Y", "Frec_Y", "Amp_Z", "Frec_Z"]
-    p4 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co", "Acc_X", "Acc_Y", "Acc_Z", "Rgyr_X", "Rgyr_Y", "Rgyr_Z"]
+    p3 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co", "RMS", "Amp_X", "Frec_X", "Amp_Y", "Frec_Y", "Amp_Z",
+          "Frec_Z"]
 
-    p = [p0, p1, p2, p3, p4]
+    p = [p0, p1, p2, p3]
 
     try:
         return protocolFunc(protocol, p[protocol])(data)
     except Exception:
         print("Data unpacking Error:", traceback.format_exc())
         return None
-
-
