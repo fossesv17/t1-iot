a52b143b723cf1335352b1a0614f6cc0306cae47
David Parada <david.parada.munoz@gmail.com>
2023-12-05 23:44:19 -0300

    Packing & Unpacking

--- codigo_rasp/packet_parser.py
+++ codigo_rasp/packet_parser.py
@@ -26,18 +26,20 @@ def packing(headers, data):
 def unpacking(packet):
     headers = getDictHeader(packet)
     protocol = headers['ID_protocol']
+    #print(packet[:37])
     values = dataToDict(protocol, packet[37:])
     return headers, values
 
 def getDictHeader(packet):
     header = packet[:37]
+    #print(header)
     header = headerToDict(header)
     return header
 
 
 def headerToDict(data):
     ID_Device, MAC, transport_layer, protocol, len_msg = unpack("<8s17s3i", data)
-    dict = {"ID_device": ID_Device.decode(), "MAC": MAC.decode(), "ID_protocol": protocol,
+    dict = {"ID_device": ID_Device.decode().rstrip("\x00"), "MAC": MAC.decode().rstrip("\x00"), "ID_protocol": protocol,
             "Transport_layer": transport_layer,
             "length": len_msg}
     return dict
@@ -98,90 +100,84 @@ def dictToData(headers, dict):
                     rms, ampx, frecx, ampy, frecy, ampz, frecz)
 
 
-def parseData(header, packet):
-    dictData = dataToDict(header["ID_protocol"], packet)
-    if dictData is not None:
-        insert_to_Datos(header, dictData)
-    return None if dictData is None else {**header, **dictData}
-
-
-def protocolUnpack(protocol: int, data):
-    protocol_unpack = ["<H", "<H19s", "<H19s4i", "<H19s4ifffffff"]
-    return unpack(protocol_unpack[protocol], data)
-
-
-def dataToDict(protocol: int, data):
+def dataToDict(protocol, data):
     if protocol not in [0, 1, 2, 3]:
         print("Error: protocol doesnt exist")
         return None
+    
+    #print(data)
+    data_dict = {}
 
-    def protocolFunc(protocol, keys):
-        def p(data):
-            unp = protocolUnpack(protocol, data)
-            data_dict = {}
-            for (key, val) in zip(keys, unp):
-                if key == "Timestamp":
-                    data_dict[key] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                else:
-                    data_dict[key] = val
-            return data_dict
-        return p
+    if protocol == 0:
+        p0 = ["Batt_level"]
 
-    p0 = ["Batt_level"]
-    p1 = ["Batt_level", "Timestamp"]
-    p2 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co"]
-    p3 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co", "RMS", "Amp_X", "Frec_X", "Amp_Y", "Frec_Y", "Amp_Z",
-          "Frec_Z"]
+        batt_lvl = unpack("<H", data)
 
-    p = [p0, p1, p2, p3]
+        data_dict[p0[0]] = batt_lvl
 
-    try:
-        return protocolFunc(protocol, p[protocol])(data)
-    except Exception:
-        print("Data unpacking Error:", traceback.format_exc())
-        return None
+        return data_dict
+    
+    elif protocol == 1:
+        p1 = ["Batt_level", "Timestamp"]
+        
+        batt_lvl, timestmp = unpack("<H19s", data)
+        data1 = [batt_lvl, timestmp]
 
+        for i in range(0, len(p1)):
+            data_dict[p1[i]] = data1[i]
 
-headers_datos_0 = {"ID_device": 'Harry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 0,
-                   "Transport_layer": 1, "length": 87}
+        return data_dict
+    
+    elif protocol == 2:
+        p2 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co"]
 
-datos_values = {"Batt_level": 80, "Timestamp": '2023-10-08 04:05:06', "Temp": 15, "Press": 1100,
-                          "Hum": 55, "Co": 176, "RMS": 0.009, "Amp_X": 0.1, "Frec_X": 30.3, "Amp_Y": 0.05,
-                          "Frec_Y": 59.1, "Amp_Z": 0.009, "Frec_Z": 90.2}
+        batt_lvl, timestmp, temp, press, hum, co = unpack("<H19s4i", data)
 
-len1 = 0
-for key,val in datos_values.items():
-    if type(val) == int or type(val) == float:
-        len1 += len(str(val))
+        data2 = [batt_lvl, timestmp, temp, press, hum, co]
+
+        for i in range(0, len(p2)):
+            data_dict[p2[i]] = data2[i]
+
+        return data_dict
+    
+    elif protocol == 3:
+        p3 = ["Batt_level", "Timestamp", "Temp", "Press", "Hum", "Co", "RMS", "Amp_X", "Frec_X", "Amp_Y", "Frec_Y", "Amp_Z",
+                "Frec_Z"]
+        
+        batt_lvl, timestmp, temp, press, hum, co, rms, ampx, frecx, ampy, frecy, ampz, frecz = unpack("<H19s4ifffffff", data)
+
+        data3 = [batt_lvl, timestmp, temp, press, hum, co, rms, ampx, frecx, ampy, frecy, ampz, frecz]
+
+        for i in range(0, len(p3)):
+            data_dict[p3[i]] = data3[i]
+
+        return data_dict
+
     else:
-        len1 += len(val)
+        print("Error")
+            
 
-for key,val in headers_datos_0.items():
-    if type(val) == int or type(val) == float:
-        len1 += len(str(val))
-    else:
-        len1 += len(val)
 
-print(len1)
 
-#packed = packing(headers_datos_0, datos_values)
+# Probando si funciona el acceso a la base de datos desde otro archivo:
 
-#print(packed)
-
-#unpacked = unpacking(packed)
-
-#print(unpacked)
-
-headers_datos_0 = {"ID_device": 'Barry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 3,
-                        "Transport_layer": '1', "length": 27}
+headers_datos_0 = {"ID_device": 'Barry', "MAC": '2C:41:A1:27:09:57', "ID_protocol": 1,
+                        "Transport_layer": '1', "length": 51}
 
 insert_to_datos_values = {"Batt_level": 75, "Timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "Temp": 15, "Press": 1100,
                             "Hum": 55, "Co": 176, "RMS": 0.009, "Amp_X": 0.1, "Frec_X": 30.3, "Amp_Y": 0.05,
                             "Frec_Y": 59.1, "Amp_Z": 0.009, "Frec_Z": 90.2}
 
-insert_to_Datos(headers_datos_0, insert_to_datos_values)
-insert_to_Logs(headers_datos_0, insert_to_datos_values)
-insert_to_Configuracion(headers_datos_0)
-config = get_current_config()
+packed = packing(headers_datos_0, insert_to_datos_values)
 
-print(config)
+#print(packed)
+
+headers, values = unpacking(packed)
+
+print(headers)
+print(values)
+
+#insert_to_Datos(headers_datos_0, insert_to_datos_values)
+#insert_to_Logs(headers_datos_0, insert_to_datos_values)
+#insert_to_Configuracion(headers_datos_0)
+#config = get_current_config()
