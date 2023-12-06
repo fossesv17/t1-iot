160b41ff057f177968363cb91058acca1b4ba28f
Paula Ovalle <paula.alejandraovalle42@gmail.com>
2023-12-06 19:55:00 -0300

    intento de api

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ from peewee import PostgresqlDatabase
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
+from modelos import insert_to_Configuracion, Configuracion, get_current_config, get_config_by_id, get_all_config, update_config_by_id, delete_config_by_id
 
 # -------------------------
 # Conexion a la base de datos
@@ -50,14 +51,22 @@ class SetSetting(BaseModel):
     setting_id: int
     value: float
 
+class ResponseExample(BaseModel):
+    message: str
+
 
 @app.get("/ejemplo/")
 async def get_items(database: PostgresqlDatabase = Depends(get_database)):
     # Aqui pueden acceder a la base de datos y hacer las consultas que necesiten
+
+    
+    # Luego pueden usar la base de datos
+    tables = database.get_tables()
+
     return {"message": "Hello World"}
 
 
-@app.post("/ejemplo/")
+@app.post("/ejemplo/", response_model=ResponseExample)
 async def create_item(setting: SetSetting, database: PostgresqlDatabase = Depends(get_database)):
     # aqui reciben un objeto de tipo Setting
     setting_dict = setting.dict()
@@ -66,3 +75,20 @@ async def create_item(setting: SetSetting, database: PostgresqlDatabase = Depend
     tables = database.get_tables()  # esto es solo un ejemplo
     print(tables)
     return {"message": "Hello World"}
+
+
+#para tomar los datos de la configuracion actual
+@app.get("/configuracion/", response_model=Configuracion)
+async def get_current_config(database: PostgresqlDatabase = Depends(get_database)):
+    return get_current_config(database)
+
+
+#para modificar los datos de la configuracion actual
+@app.put("/configuracion/", response_model=Configuracion)
+async def update_current_config(config: Configuracion, database: PostgresqlDatabase = Depends(get_database)):
+    return update_config_by_id(database, config)
+
+
+
+
+
